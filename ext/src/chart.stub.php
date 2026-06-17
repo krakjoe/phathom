18fce3c6f35b816be841fe008d4d123f88f01547
Joe Watkins <krakjoe@php.net>
2026-06-17 05:53:09 +0200

    Remove ext for proper subtree setup

--- ext/src/chart.stub.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace pharos\phathom\Earley {
-	final class Chart {
-		public function __construct(
-			\pharos\phathom\Grammar $grammar,
-			\pharos\phathom\Interface\Buffer $input) {}
-	}
-}
-?>
