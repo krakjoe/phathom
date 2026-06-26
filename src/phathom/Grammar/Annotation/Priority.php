7b102b05cabd512ad192678cfd2f94dbf413dfbd
Joe Watkins <krakjoe@php.net>
2026-06-26 08:37:56 +0200

    licenses

--- src/phathom/Grammar/Annotation/Priority.php
+++ src/phathom/Grammar/Annotation/Priority.php
@@ -1,4 +1,16 @@
 <?php declare(strict_types=1);
+/*
+  +----------------------------------------------------------------------+
+  | phathom                                                              |
+  +----------------------------------------------------------------------+
+  | Copyright (c) Joe Watkins 2026                                       |
+  +----------------------------------------------------------------------+
+  | This source file is subject to the BSD 3-Clause License bundled     |
+  | with this package in the file LICENSE.                               |
+  +----------------------------------------------------------------------+
+  | Author: krakjoe                                                      |
+  +----------------------------------------------------------------------+
+ */
 
 namespace pharos\phathom\Grammar\Annotation {
     use \pharos\phathom\Grammar\Interface\Annotation as AnnotationInterface;
